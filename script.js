101f3d194e8fa4c105846158bab36ae2c5246e6a
ericrothmuller <eric@skymarketingco.com>
2022-07-05 23:05:18 -0700

    Added search button event listener that adds city name to local storage and appends the city name to the list area beneath it.

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Search Button
+
+var searchForm = document.getElementById("searchform");
+
+var cityNameForm = document.getElementById("citynameform");
+
+var cityNameListArea = document.getElementById("cityNameList");
+
+function searchFormSubmitted(event) {
+    event.preventDefault();
+    if (cityNameForm.value) {
+        // console.log(cityNameForm.value.toUpperCase());
+        var cityNameUppercased = cityNameForm.value.toUpperCase();
+        var inputKey = "CityName";
+        var inputValue = cityNameUppercased;
+        localStorage.setItem(inputKey, inputValue);
+        cityNameListArea.innerHTML += "<li>" + cityNameUppercased + "</li>";
+    } else {
+        alert("Must enter city name!")
+    }
+}
+
+searchForm.addEventListener("submit", searchFormSubmitted);
